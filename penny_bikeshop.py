27c54fe62d3c2c818ac2823de7979eb1abdfd6b2
Margie Roswell <mroswell@gmail.com>
2014-11-20 09:40:54 -0500

    Clean up Bikeshop inventory display

--- penny_bikeshop.py
+++ penny_bikeshop.py
@@ -6,14 +6,23 @@ explorer = Bicycle('Explorer',20,750)
 scooter = Bicycle('Scooter',5, 300)
 folder = Bicycle('Folder',13, 1600)
 friday = Bicycle('Bike Friday', 19,1400)
+
 print schwinn.name
 print "Weighs", schwinn.weight, "lbs"
 print "Costs $", schwinn.production_cost, "to produce"
+
 penny_shop = Bikeshop("Penny's Bikes", [schwinn, bikee, explorer, scooter, folder, friday])
 joanne = Customer('Joanne', 200)
 anne = Customer('Anne', 500)
 jane = Customer('Jane', 1690)
 customers = [joanne, jane, anne]
+
+print "-----------"
+print "Penny's Inventory"
+print "-----------"
+for bike in penny_shop.inventory:
+    print "-", bike.name
+
 for customer in customers:
   print '---------------'
   print customer.name
@@ -21,30 +30,16 @@ for customer in customers:
   for bike in penny_shop.inventory:
     if customer.budget >= bike.production_cost * 1.2:
       print "-", bike.name
-  print "-----------"
-  print "Penny's Inventory"
-  print "-----------"
 
-  for bike in penny_shop.inventory:
-    print "-", bike.name
 
-  print "psi", penny_shop.inventory
   for bike in penny_shop.inventory:
     if customer.budget >= bike.customer_cost:
       customer.owned = bike
-      pass
-  print "owned bike: ", customer.owned.name, customer.owned.production_cost
-  print "-----------"
-  print "Penny's Inventory"
-  print "-----------"
-  for bike in penny_shop.inventory:
-     print "-", bike.name
-
   penny_shop.sell_bike(customer.owned)
 
+  print "owned bike: ", customer.owned.name, customer.owned.production_cost
 print "-----------"
 print "Penny's Inventory"
 print "-----------"
 for bike in penny_shop.inventory:
-  print "-", bike.name
-
+   print "-", bike.name
